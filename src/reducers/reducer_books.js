6c9d2f7b48fed05ec05aeb78b222584f2d914ce4
Aditya Sanghi <aditya@punchh.com>
2016-04-29 13:45:51 +0530

    book redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript', pages: 101 },
+    {title: 'Happy Potter', pages: 39 },
+    {title: 'Dark Tower', pages: 85 },
+    {title: 'Eloquent Ruby', pages: 1 },
+  ];
+};
